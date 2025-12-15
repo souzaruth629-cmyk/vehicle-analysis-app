66c162f82ea69642ea07d59ec8f3d53f5921f389
souzaruth629-cmyk <souzaruth629@gmail.com>
2025-12-15 17:41:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,28 @@ car_data = pd.read_csv('vehicles_us.csv')
 
 # Segundo: criar o cabeçalho
 st.header('Dashboard de Análise de Veículos')
+ #Criar slider para filtrar por faixa de preço
+price_range = st.slider(
+    'Selecione a faixa de preço:',
+    min_value=int(car_data['price'].min()),
+    max_value=int(car_data['price'].max()),
+    value=(int(car_data['price'].min()), int(car_data['price'].max()))
+  # Filtrar dados baseado no slider
+filtered_data = car_data[(car_data['price'] >= price_range[0]) & 
+                        (car_data['price'] <= price_range[1])]
+)# Criar gráfico de dispersão
+st.subheader('Relação entre Preço e Ano do Veículo')
+fig_scatter = px.scatter(dados_do_carro, x='model_year', y='price', 
+                        title='Preço vs Ano do Carro')
+st.plotly_chart(fig_scatter)
+
+
+
+# Criar histograma da distribuição de preços
+fig_price_dist = px.histogram(car_data, 
+                             x='price', 
+                             title='Distribuição de Preços dos Carros',
+                             labels={'price': 'Preço (USD)', 'count': 'Quantidade de Carros'},
+                             nbins=30)  # número de barras no histograma
+
+st.plotly_chart(fig_price_dist)
